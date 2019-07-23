f93cc5003d1bf4e9ab9813ceef8dcc251cd3af21
MinKyeongTae <kyeongtea92@gmail.com>
2019-07-23 18:00:40 +0900

    Study for Asynchronus, Synchronous precess

--- Example/step1/RxSwiftIn4Hours/AsyncViewController.swift
+++ Example/step1/RxSwiftIn4Hours/AsyncViewController.swift
@@ -29,13 +29,27 @@ class AsyncViewController: UIViewController {
 
     // MARK: - IBAction
 
+    // MARLL Synchronous Task Method
     @IBAction func onLoadSync(_ sender: Any) {
         let image = loadImage(from: IMAGE_URL)
         imageView.image = image
     }
 
+    // MARK: Asynchronous Task Method
     @IBAction func onLoadAsync(_ sender: Any) {
         // TODO: async
+        DispatchQueue.global().async { // Concurrency, 비동기방식으로 동시에 실행한다.
+            guard let url = URL(string: self.IMAGE_URL) else { return } // 데이터 요청할 URL을 준비한다.
+            guard let data = try? Data(contentsOf: url) else { return } // URL에 대한 데이터를 요정한다.
+            
+            let image = UIImage(data: data) // 데이터를 받았으면 해당 테이터에 맞는 이미지를 가져온다.
+            
+            DispatchQueue.main.async {
+                // ★ 비동기로 UI를 건드리는 경우에는 메인스레드 내에서 건드려주어야 한다.
+                self.imageView.image = image // imageView의 이미지를 셋팅 해준다.
+            }
+            
+        }
     }
 
     private func loadImage(from imageUrl: String) -> UIImage? {
