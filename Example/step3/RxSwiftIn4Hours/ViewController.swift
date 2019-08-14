ff599acdb83fb24b4b6043c9e5605b2943165575
MinKyeongTae <kyeongtea92@gmail.com>
2019-08-14 20:34:37 +0900

    Study for... How to use Observable in TextField UI

--- Example/step3/RxSwiftIn4Hours/ViewController.swift
+++ Example/step3/RxSwiftIn4Hours/ViewController.swift
@@ -18,10 +18,10 @@ class ViewController: UIViewController {
         bindUI()
     }
 
-    // MARK: - IBOutler
+    // MARK: - IBOutlet
 
-    @IBOutlet var idField: UITextField!
-    @IBOutlet var pwField: UITextField!
+    @IBOutlet var emailTextField: UITextField!
+    @IBOutlet var pwTextField: UITextField!
     @IBOutlet var loginButton: UIButton!
     @IBOutlet var idValidView: UIView!
     @IBOutlet var pwValidView: UIView!
@@ -29,19 +29,42 @@ class ViewController: UIViewController {
     // MARK: - Bind UI
 
     private func bindUI() {
+        //
         // id input +--> check valid --> bullet
         //          |
         //          +--> button enable
         //          |
         // pw input +--> check valid --> bullet
+
+        // 1) textView.delegate.self
+        // 2) Using textView, textField's textchanged Delegate Method
+        // 3) Verification both email and password text
+        // 4) set Button Enabled status whenever the text value changed
+
+        // orEmpty 값이 비어있는지 체크하는 변수
+        emailTextField.rx.text.orEmpty
+            .map(checkEmailValid)
+            .subscribe(onNext: { isEmpty in
+                self.idValidView.isHidden = isEmpty // 만약 텍스트가 비어있다면 false로 빨간 구슬이 나온다.
+            })
+            .disposed(by: disposeBag)
+
+        pwTextField.rx.text.orEmpty
+            .map(checkPasswordValid)
+            .subscribe(onNext: { isEmpty in
+                self.pwValidView.isHidden = isEmpty
+            })
+            .disposed(by: disposeBag)
     }
 
     // MARK: - Logic
 
+    /// * 이메일 포맷 여부 체크 메서드
     private func checkEmailValid(_ email: String) -> Bool {
         return email.contains("@") && email.contains(".")
     }
 
+    /// * 패스워드 포맷 여부 체크 메서드
     private func checkPasswordValid(_ password: String) -> Bool {
         return password.count > 5
     }
