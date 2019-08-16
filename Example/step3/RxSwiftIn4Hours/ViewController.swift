b28d8e3c2cf47a42e6a9ff57e7d327d216490b08
MinKyeongTae <kyeongtea92@gmail.com>
2019-08-16 20:47:48 +0900

    Study For How to Use RxSwift Subject

--- Example/step3/RxSwiftIn4Hours/ViewController.swift
+++ Example/step3/RxSwiftIn4Hours/ViewController.swift
@@ -11,11 +11,43 @@ import RxSwift
 import UIKit
 
 class ViewController: UIViewController {
+    // MARK: - Property
+
     var disposeBag = DisposeBag()
+
+    // ✭ Subject
+    // - Subject의 종류 : Async Subject, BehaviorSubject, publishSubject, ReplaySubject
+
+    // ❋ Behavior Subject : 스스로 데이터를 발생 가능 + Subscribe 가능 Observable
+    // - 최초 SubScribe 이후, Default값으로 상태를 지켜보며 기다리다가 어떤 subscribe가 발생하면 그 Subscribe에 최근 데이터값을 넘겨준다.
+    // 해당 Subject가 종료 되면 이후 라는 스트림에서 Subscribe를 해도 해당 Subject는 종료된다.
+    // ✓ Bullet View만 상황을 지켜보다가 Enable or Disable 여부를 판단하여 변경할 수 있다.
+    // * value: false 는 default 값
+
+    // ❋ Async Subject : 해당 Subject가 종료되는 시점의 맨 마지막 전달된 데이터를 SubScribe한 Stream들에 전달시킨다.
+
+    // ❋ Publish Subject : "최초 Default값이 없다." 데이터가 생성되면 그때 데이터를 전달한다.
+    // - 데이터가 생성될 때마다 해당 Subject를 Subscribe한 놈들에게 전부 해당 데이터를 전달한다.
+
+    // ❋ Replay Subject : "최초 Default값이 없다." 데이터가 생성되면 지금까지 생성했던 데이터를 한꺼번에 전달한다.
+    // - 마블 3개가 지나간 뒤 다른 Subscribe가 진행되면 해당 Stream에 이전 마블 3개를 전부 전달한다. 이후 생성되는 데이터는 모든 Stream에 전달된다.
+
+    let emailInputText: BehaviorSubject<String> = BehaviorSubject(value: "")
+    let emailValid: BehaviorSubject<Bool> = BehaviorSubject(value: false)
+    let pwInputText: BehaviorSubject<String> = BehaviorSubject(value: "")
+    let pwValid: BehaviorSubject<Bool> = BehaviorSubject(value: false)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        bindUI()
+
+        // MARK: 1) Subject 없이 구현
+
+        //        bindUI()
+
+        // MARK: 2) Subject와 혼용하여 구현
+
+        bindInput()
+        bindOutput()
     }
 
     // MARK: - IBOutlet
@@ -23,10 +55,58 @@ class ViewController: UIViewController {
     @IBOutlet var emailTextField: UITextField!
     @IBOutlet var pwTextField: UITextField!
     @IBOutlet var loginButton: UIButton!
-    @IBOutlet var idValidView: UIView!
+    @IBOutlet var emailValidView: UIView!
     @IBOutlet var pwValidView: UIView!
 
     // MARK: - Bind UI
+
+    private func bindInput() {
+        // Input : 아이디 입력, 비번 입력
+
+        // * 이메일 텍스트필드의 데이터도 각각의 Subject로 bind하여 감시한다.
+        emailTextField.rx.text.orEmpty
+            .bind(to: emailInputText)
+            .disposed(by: disposeBag)
+
+        // 텍스트필드의 데이터 판별 Subject에도 bind처리로 감시한다.
+        emailInputText
+            .map(checkEmailValid)
+            .bind(to: emailValid)
+            .disposed(by: disposeBag)
+
+        // * 비번 텍스트필스의 데이터도 각각의 Subject와 binding 하여 감시를 한다.
+        pwTextField.rx.text.orEmpty
+            .bind(to: pwInputText)
+            .disposed(by: disposeBag)
+
+        pwInputText
+            .map(checkPasswordValid)
+            .bind(to: pwValid)
+            .disposed(by: disposeBag)
+    }
+
+    private func bindOutput() {
+        // Output : Bullet, Login Button Enabled or Disabled
+        emailValid.subscribe(onNext: { bool in
+            self.emailValidView.isHidden = bool
+        })
+            .disposed(by: disposeBag)
+
+        pwValid.subscribe(onNext: { bool in
+            self.pwValidView.isHidden = bool
+        })
+            .disposed(by: disposeBag)
+
+        Observable.combineLatest(
+            emailValid,
+            pwValid,
+            resultSelector: { $0 && $1 }
+        )
+        .subscribe(onNext: { bool in
+            self.loginButton.isEnabled = bool
+        })
+        .disposed(by: disposeBag)
+    }
 
     private func bindUI() {
         //
@@ -36,41 +116,86 @@ class ViewController: UIViewController {
         //          |
         // pw input +--> check valid --> bullet
 
+        // MARK: 따로 Observable 만들기
+
         // 1) textView.delegate.self
         // 2) Using textView, textField's textchanged Delegate Method
         // 3) Verification both email and password text
         // 4) set Button Enabled status whenever the text value changed
 
-        // orEmpty 값이 비어있는지 체크하는 변수 + 비동기, Async하게 실행 되는 것
-        emailTextField.rx.text.orEmpty
-            .map(checkEmailValid)
-            .subscribe(onNext: { isEmpty in
-                self.idValidView.isHidden = isEmpty // 만약 텍스트가 비어있다면 false로 빨간 구슬이 나온다.
-            })
-            .disposed(by: disposeBag)
+        //        // orEmpty 값이 비어있는지 체크하는 변수 + 비동기, Async하게 실행 되는 것
+        //        emailTextField.rx.text.orEmpty
+        //            .map(checkEmailValid)
+        //            .subscribe(onNext: { isEmpty in
+        //                self.idValidView.isHidden = isEmpty // 만약 텍스트가 비어있다면 false로 빨간 구슬이 나온다.
+        //            })
+        //            .disposed(by: disposeBag)
+        //
+        //        pwTextField.rx.text.orEmpty
+        //            .map(checkPasswordValid)
+        //            .subscribe(onNext: { isEmpty in
+        //                self.pwValidView.isHidden = isEmpty
+        //            })
+        //            .disposed(by: disposeBag)
 
-        pwTextField.rx.text.orEmpty
-            .map(checkPasswordValid)
-            .subscribe(onNext: { isEmpty in
-                self.pwValidView.isHidden = isEmpty
-            })
-            .disposed(by: disposeBag)
+        // MARK: Observable 합쳐서 처리하기
 
-        /// 두 개의 Observable을 합쳐서 체크해보자
-        // ✓ CombineLatest : 다른 Observable을 결합시켜 새로운 Observable을 생성해야 할때 사용한다.
-        // ✓ Zip : 두 개 전부 데이터가 변경이 될 때 전달한다.
-        // ✓ Merge : 데이터가 들어오는대로 한번 씩 한번씩 내린다.(?)
-        Observable.combineLatest(
-            pwTextField.rx.text.orEmpty.map(checkPasswordValid), // 합쳐서 실행 할 체크 옵저바블 1
-            emailTextField.rx.text.orEmpty.map(checkEmailValid), // 합쳐서 실행 할 체크 옵저바블 2
-            resultSelector: { s1, s2 in s1 && s2 } // 옵저바블 2개가 동시에 성립하는 지 체크한다.
-        )
-        .subscribe(onNext: { bool in
-            // 두 개의 옵저바블(이메일, 비밀번호 체크)가 성립하면 로그인 버튼을 활성화 시킨다.
-            print(bool)
-            self.loginButton.isEnabled = bool
-        })
-        .disposed(by: disposeBag)
+        //        /// => 두 개의 Observable을 합쳐서 체크해보자
+        //        // ✓ CombineLatest : 다른 Observable을 결합시켜 새로운 Observable을 생성해야 할때 사용한다.
+        //        // ✓ Zip : 두 개 전부 데이터가 변경이 될 때 전달한다.
+        //        // ✓ Merge : 데이터가 들어오는대로 한번 씩 한번씩 내린다.(?)
+        //        Observable.combineLatest(
+        //            pwTextField.rx.text.orEmpty.map(checkPasswordValid), // 합쳐서 실행 할 체크 옵저바블 1
+        //            emailTextField.rx.text.orEmpty.map(checkEmailValid), // 합쳐서 실행 할 체크 옵저바블 2
+        //            resultSelector: { s1, s2 in s1 && s2 } // 옵저바블 2개가 동시에 성립하는 지 체크한다.
+        //        )
+        //        .subscribe(onNext: { bool in
+        //            // 두 개의 옵저바블(이메일, 비밀번호 체크)가 성립하면 로그인 버튼을 활성화 시킨다.
+        //            print(bool)
+        //            self.loginButton.isEnabled = bool
+        //        })
+        //        .disposed(by: disposeBag)
+
+        // MARK: 좀 더 세련되게 Observable 처리하기.
+
+        //        let emailInputObservable: Observable<String> = emailTextField.rx.text.orEmpty.asObservable()
+        //
+        //        // emailValid라는 behavior Subject에 데이터를 넘겨 데이터에 따른 판별을 실행한다.
+        //        let emailValidObservable = emailInputObservable
+        //            .map(checkEmailValid)
+        //            .bind(to: emailValid)
+        //            .disposed(by: disposeBag)
+
+        //        // Subject에 데이터 넣는 1번째 방법
+        //        emailValidObservable.subscribe(onNext: { bool in
+        //            // BehaviorSubject에 bool 데이터값을 넣어준다.
+        //            self.emailValid.onNext(bool)
+        //        })
+
+        //        // Subject에 데이터 넣는 2번째 방법
+        //        // 난 이제 emailValidObservable 데이터를 emailValid Subject로 넘길거야!
+        //        emailValidObservable.bind(to: emailValid)
+
+        //        let passwordInputObservable: Observable<String> = pwTextField.rx.text.orEmpty.asObservable()
+        //        let passwordValidObservable = passwordInputObservable.map(checkPasswordValid)
+
+        // * Behavior Subject를 사용할 때
+
+        //        // * Subject 를 사용 안하고 사용할 때
+        //        // Output : Bullet, Login Button Enable or Disable Check
+        //
+        //        emailValidObservable.subscribe(onNext: { bool in self.emailValidView.isHidden = bool })
+        //        .disposed(by: disposeBag)
+        //        passwordValidObservable.subscribe(onNext: { bool in
+        //            self.pwValidView.isHidden = bool })
+        //        .disposed(by: disposeBag)
+        //
+        //        Observable.combineLatest(
+        //            emailValidObservable,
+        //            passwordValidObservable,
+        //            resultSelector: { $0 && $1 } )
+        //            .subscribe(onNext: { bool in self.loginButton.isEnabled = bool} )
+        //            .disposed(by: disposeBag)
     }
 
     // MARK: - Logic
